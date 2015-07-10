a724c88da12a4a2340172de46ed4f83dae5ae30a
namasse69 <gaetan.chapuis@laposte.net>
2015-07-10 10:06:50 +0200

    Table SQL

---
+++ sitedevoyage.sql
@@ -0,0 +1,67 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.3.4
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 10 Juillet 2015 à 10:05
+-- Version du serveur :  5.6.15-log
+-- Version de PHP :  5.5.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `sitedevoyage`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `idInscription` int(255) NOT NULL,
+  `userName` varchar(255) NOT NULL,
+  `userFirstname` varchar(255) NOT NULL,
+  `userPseudo` varchar(255) NOT NULL,
+  `userMail` varchar(255) NOT NULL,
+  `userPass` varchar(255) NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`idInscription`, `userName`, `userFirstname`, `userPseudo`, `userMail`, `userPass`) VALUES
+(2, 'Chapuis', 'GaÃ«tan', 'Namasse', 'gaetan.chapuis@laposte.net', 'chrisol'),
+(3, 'Bouchisse', 'LoÃ¯c', 'Kyzend', 'loic.bouchisse@laposte.net', 'parkour50');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`idInscription`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `idInscription` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
